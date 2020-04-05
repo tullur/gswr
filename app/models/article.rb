875a2fdc6e7ba3b67b67ff34baa4b9901ab241db
psidae <akson.rill@gmail.com>
2020-04-05 21:44:02 +0300

    First step

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  belongs_to :user
   has_many :comments, dependent: :destroy
-  validates :title, presence: true,
-                    length: { minimum: 5 }
 
+  validates :title, presence: true,
+                    length: { minimum: 3 },
+                    allow_blank: false
+  validates :body, presence: true,
+                   length: { in: 5..100 },
+                   allow_blank: false
+
+  scope :author, ->(username) { where(user: User.where(username: username)) }
   scope :search, ->(title) { where('title LIKE ?', "%#{title}%") }
 end
---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_secure_password
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :name, presence: true,
+                   uniqueness: { case_sensetive: false },
+                   length: { in: 3..15 },
+                   allow_blank: false
+
+  EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensetive: false },
+                    format: { with: EMAIL_REGEXP }
+end
