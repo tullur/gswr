b56257978ca6fe9371e213612ab14ac875107c56
psidae <akson.rill@gmail.com>
2020-04-07 21:52:59 +0300

    View update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def index
     @users = User.all
@@ -47,5 +48,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email,
                                  :password, :password_confirmation, :avatar)
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = 'You can only edit or delete your own articles'
+      redirect_to root_path
+    end
   end
 end
